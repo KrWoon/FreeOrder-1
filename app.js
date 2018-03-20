903f7d639cb303624a0b15241c987f56725928ee
bcp0109 <bcp0109@gmail.com>
2018-03-21 06:09:16 +0900

    dsfg

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
     res.render('my_first_ejs', data);
 });
 
-app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
     console.log('Server On!');
 });
