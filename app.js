20ed4308733a073b26395af4ceed395c1797be43
bcp0109 <bcp0109@gmail.com>
2018-03-24 00:45:16 +0900

    asdf

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
-
 var app = express();
+
+// router
+var main = require('./router/main');
 
 // connect database
 var conn = mysql.createConnection({
@@ -15,6 +17,9 @@ var conn = mysql.createConnection({
 });
 conn.connect();
 
+// router use
+app.use ('/main', main);
+
 // set model
 
 // set view
@@ -22,15 +27,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
 
 // set middlewares
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 // set routes
 
 
+// route URL
 app.get('/', function (req, res) {
-    res.send("hello");
+    res.sendFile(__dirname + "/public/main.html");
 });
 
 app.get('/login', function (req, res) {
@@ -39,8 +45,27 @@ app.get('/login', function (req, res) {
 
 app.post('/email_post', function(req,res) {
     console.log(req.body.email);
-    res.send("<h1>Welcome! " + req.body.email + "</h1>");
+    //res.send("<h1>Welcome! " + req.body.email + "</h1>");
+    res.render('email', {'email' : req.body.email});
 });
+
+app.post('/ajax_send_email', function(req, res){
+    var email = req.body.email;
+    var responseData = {};
+
+    var query = conn.query('select name from tempUser where email="' + email + '"', function (err, rows) {
+        if (err) throw err;
+        if (rows[0]) {
+            console.log(rows[0])
+            responseData.result = "ok";
+            responseData.name = rows[0].name;
+        } else {
+            responseData.result = "none";
+            responseData.name = "";
+        }
+        res.json(responseData)
+    })
+})
 
 
 // start server
