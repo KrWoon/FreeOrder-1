bc6b2170eb6dc64304a1f4a55c2977feb6bb8a39
bcp0109 <bcp0109@gmail.com>
2018-05-19 17:16:24 +0900

    d

--- routes/passport_routes/order.js
+++ routes/passport_routes/order.js
@@ -18,8 +18,12 @@ module.exports = function(io) {
 
     // add mobile data test
     router.post('/mobile/test', function(req, res) {
-        var order = req.body;
+        var order = req.body.mobileOrders;
         var totalOrder = [];
+
+        // jiwoon
+        io.sockets.emit('customOrder', order);
+        res.write("req.body Ok");
 
         // insert to ordereditem table
         var First_func = function(){
@@ -83,19 +87,84 @@ module.exports = function(io) {
         };
         
         First_func();
+        res.end();
     });
 
 
     //add mobile data
     router.post('/mobile', function(req, res) {
-        var inputData = req.body;
+        // var inputData = req.body;
         
+        var order = req.body;
+        var totalOrder = [];
+
         // jiwoon
-        io.sockets.emit('customOrder', inputData.mobileOrders);
+        io.sockets.emit('customOrder', order);
         res.write("req.body Ok");
 
 
-        console.log('who get in here post /users');
+        // insert to ordereditem table
+        var First_func = function(){
+            var sql = 'INSERT INTO ordereditem (Email, Restaurant_Code) VALUES (?, ?)';
+
+            pool.getConnection(function(err, conn) {
+                if(err) throw err;
+
+                conn.query(sql, [order[0].Email, order[0].Restaurant_Code], function(err, results) {
+                    console.log('insert ordereditem complete');
+                    Second_func();
+                });
+            });
+        };
+        
+        // get order_code and insert to orderedmenu table
+        var Second_func = function(){
+            var sql = 'SELECT Order_Code FROM ordereditem WHERE Email = ? AND Restaurant_Code = ?';
+
+            pool.getConnection(function(err, conn) {
+                if(err) throw err;
+
+                conn.query(sql, [order[0].Email, order[0].Restaurant_Code], function(err, orderCodes) {
+                    var recent = orderCodes[orderCodes.length - 1];
+
+                    for(var i=0; i<order.length; i++) {
+                        if(order[i].MenuOption_CodeList != 0) {
+                            for(var j=0; j<order[i].MenuOption_CodeList.length; j++) {
+                                orderedMenu = {
+                                    Order_Code: recent.Order_Code,
+                                    Sequence: i,
+                                    Menu_Code: order[i].Menu_Code,
+                                    MenuOption_Code: order[i].MenuOption_CodeList[j].MenuOption_Code
+                                }
+                                totalOrder.push(orderedMenu);
+                            }
+                        } else {
+                            orderedMenu = {
+                                Order_Code: recent.Order_Code,
+                                Sequence: i,
+                                Menu_Code: order[i].Menu_Code
+                            }
+                            totalOrder.push(orderedMenu);
+                        }
+                    }
+
+                    sql = 'INSERT INTO orderedmenu SET ?';
+                    for(var i=0; i<totalOrder.length; i++) {                        
+                        conn.query(sql, totalOrder[i], function(err, results) {
+                            if(err) {
+                                console.log(err);
+                            } else {
+                                console.log('complete');
+                            }
+                        })    
+                    }
+
+                    conn.release();
+                });
+            });
+        };
+        
+        First_func();
         res.end();
     });
 
