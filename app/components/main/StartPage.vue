3afa3342cd02f8747788bae3792f8c451141e556
freedomchurl <freedomchurl@naver.com>
2018-06-05 15:28:33 +0900

    adf

--- app/components/main/StartPage.vue
+++ app/components/main/StartPage.vue
@@ -48,6 +48,7 @@ export default {
             this.axios.get('/index')
             .then(res => {
                 this.info = res.data;
+		console.log('hi');
             })
             .catch(err => console.log(err));
         }
